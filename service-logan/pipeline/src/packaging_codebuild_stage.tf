f007673c4e086fe875b6b9ba674509bd46aeec9f
kief <kmorris@thoughtworks.com>
2018-03-25 18:12:28 +0100

    Make artefact filename generic

--- service-logan/pipeline/src/packaging_codebuild_stage.tf
+++ service-logan/pipeline/src/packaging_codebuild_stage.tf
@@ -1,9 +1,9 @@
 
 resource "aws_codebuild_project" "packaging-project" {
-  name         = "${var.role}-${var.component}-${var.estate_id}-Packaging_Project"
-  description  = "CodeBuild project to package the ${var.role}-${var.component}-${var.estate_id} codebase"
-  build_timeout      = "5"
-  service_role = "${aws_iam_role.packaging_codebuild_role.arn}"
+  name          = "${var.role}-${var.component}-${var.estate_id}-Packaging_Project"
+  description   = "CodeBuild project to package the ${var.role}-${var.component}-${var.estate_id} codebase"
+  build_timeout = "5"
+  service_role  = "${aws_iam_role.packaging_codebuild_role.arn}"
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
